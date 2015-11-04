42667e2f5228cab19047a3c0930f1bf8e37ab665
Gilles <gillesvdp@gmail.com>
2015-11-04 12:30:22 +0000

    ViewController files creation and connection with storyboard

---
+++ OnTheMap/PostInformationViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PostInformationViewController.swift
+//  OnTheMap
+//
+//  Created by Gilles on 11/4/15.
+//  Copyright © 2015 gillesvdp. All rights reserved.
+//
+
+import UIKit
+
+class PostInformationViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
