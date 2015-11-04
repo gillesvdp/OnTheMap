03d6350e7d48fe867606db6fb9cabc20367ef9c6
Gilles <gillesvdp@gmail.com>
2015-11-04 14:16:22 +0000

    LoginVC, basic behavior complete
    UITextFields completed; Sign-up button works. Need to develop login
    authorization process.

--- OnTheMap/LoginViewController.swift
+++ OnTheMap/LoginViewController.swift
@@ -8,25 +8,46 @@
 
 import UIKit
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
-    // Outlets
-    
-    @IBOutlet weak var loginBackgroundOnlyOutlet: UITextField!
+    // IBOutlets
+    @IBOutlet weak var emailBackgroundOnlyOutlet: UITextField!
     @IBOutlet weak var passwordBackgroundOnlyOutlet: UITextField!
+    @IBOutlet weak var emailUITextFieldOutlet: UITextField!
+    @IBOutlet weak var passwordUITextFieldOutlet: UITextField!
+    @IBOutlet weak var loginButtonOutlet: UIButton!
+    @IBOutlet weak var signupButtonOutlet: UIButton!
     
+    
+    // Button Actions
+    @IBAction func loginButtonPressed(sender: AnyObject) {
+        
+    }
+    
+    
+    @IBAction func signupButtonPressed(sender: AnyObject) {
+        // Go to sign-up page of Udacity in Safari
+        UIApplication.sharedApplication().openURL(NSURL(string:"https://www.udacity.com/account/auth#!/signup")!)
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        loginBackgroundOnlyOutlet.enabled = false
+        
+        // Disabling the 2 UITextFields that are being used as background only
+        emailBackgroundOnlyOutlet.enabled = false
         passwordBackgroundOnlyOutlet.enabled = false
+    
+        // Set placeholder text color to white for the 2 UITextFields
+        // Source: http://stackoverflow.com/questions/25679075/set-textfield-placeholder-color-progrmatically-in-swift
+        
+        emailUITextFieldOutlet.attributedPlaceholder = NSAttributedString(string: emailUITextFieldOutlet.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
+        passwordUITextFieldOutlet.attributedPlaceholder = NSAttributedString(string: passwordUITextFieldOutlet.placeholder!, attributes: [NSForegroundColorAttributeName: UIColor.whiteColor()])
         
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
+        view.endEditing(true)
+        super.touchesBegan(touches, withEvent: event)
     }
 
 
