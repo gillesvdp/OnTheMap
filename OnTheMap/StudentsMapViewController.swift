34f23c92a83cf77dc0872052fb2eaac0e9fdea83
Gilles <gillesvdp@gmail.com>
2015-11-08 09:06:28 +0000

    Added map and basic function to Map View

--- OnTheMap/StudentsMapViewController.swift
+++ OnTheMap/StudentsMapViewController.swift
@@ -7,29 +7,115 @@
 //
 
 import UIKit
+import MapKit
 
-class StudentsMapViewController: UIViewController {
+class StudentsMapViewController: UIViewController, MKMapViewDelegate {
 
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        let locations = hardCodedLocationData()
+        var annotations = [MKPointAnnotation]()
+        
+        for dictionary in locations {
+            let lat = CLLocationDegrees(dictionary["latitude"] as! Double)
+            let long = CLLocationDegrees(dictionary["longitude"] as! Double)
+            
+            let coordinate = CLLocationCoordinate2D(latitude: lat, longitude: long)
+            
+            let first = dictionary["firstName"] as! String
+            let last = dictionary["lastName"] as! String
+            let mediaURL = dictionary["mediaURL"] as! String
+            
+            let annotation = MKPointAnnotation()
+            annotation.coordinate = coordinate
+            annotation.title = "\(first) \(last)"
+            annotation.subtitle = mediaURL
+        
+            annotations.append(annotation)
+        }
+        self.mapView.addAnnotations(annotations)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func mapView(mapView: MKMapView, viewForAnnotation annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        let reuseId = "pin"
+        var pinView = mapView.dequeueReusableAnnotationViewWithIdentifier(reuseId) as? MKPinAnnotationView
+        
+        if pinView == nil {
+            pinView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: reuseId)
+            pinView!.canShowCallout = true
+            pinView!.pinColor = .Red
+            pinView!.rightCalloutAccessoryView = UIButton(type: .DetailDisclosure)
+        }
+        else {
+            pinView!.annotation = annotation
+        }
+        
+        return pinView
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func mapView(mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+        if control == view.rightCalloutAccessoryView {
+            let app = UIApplication.sharedApplication()
+            if let toOpen = view.annotation?.subtitle! {
+                app.openURL(NSURL(string: toOpen)!)
+            }
+        }
     }
-    */
+    
+    func hardCodedLocationData() -> [[String : AnyObject]] {
+        return  [
+            [
+                "createdAt" : "2015-02-24T22:27:14.456Z",
+                "firstName" : "Jessica",
+                "lastName" : "Uelmen",
+                "latitude" : 28.1461248,
+                "longitude" : -82.75676799999999,
+                "mapString" : "Tarpon Springs, FL",
+                "mediaURL" : "www.linkedin.com/in/jessicauelmen/en",
+                "objectId" : "kj18GEaWD8",
+                "uniqueKey" : 872458750,
+                "updatedAt" : "2015-03-09T22:07:09.593Z"
+            ], [
+                "createdAt" : "2015-02-24T22:35:30.639Z",
+                "firstName" : "Gabrielle",
+                "lastName" : "Miller-Messner",
+                "latitude" : 35.1740471,
+                "longitude" : -79.3922539,
+                "mapString" : "Southern Pines, NC",
+                "mediaURL" : "http://www.linkedin.com/pub/gabrielle-miller-messner/11/557/60/en",
+                "objectId" : "8ZEuHF5uX8",
+                "uniqueKey" : 1,
+                "updatedAt" : "2015-03-11T03:23:49.582Z"
+            ], [
+                "createdAt" : "2015-02-24T22:30:54.442Z",
+                "firstName" : "Jason",
+                "lastName" : "Schatz",
+                "latitude" : 37.7617,
+                "longitude" : -122.4216,
+                "mapString" : "18th and Valencia, San Francisco, CA",
+                "mediaURL" : "http://en.wikipedia.org/wiki/Swift_%28programming_language%29",
+                "objectId" : "hiz0vOTmrL",
+                "uniqueKey" : 1,
+                "updatedAt" : "2015-03-10T17:20:31.828Z"
+            ], [
+                "createdAt" : "2015-03-11T02:48:18.321Z",
+                "firstName" : "Jarrod",
+                "lastName" : "Parkes",
+                "latitude" : 34.73037,
+                "longitude" : -86.58611000000001,
+                "mapString" : "Huntsville, Alabama",
+                "mediaURL" : "https://linkedin.com/in/jarrodparkes",
+                "objectId" : "CDHfAy8sdp",
+                "uniqueKey" : 996618664,
+                "updatedAt" : "2015-03-13T03:37:58.389Z"
+            ]
+        ]
+    }
 
 }
